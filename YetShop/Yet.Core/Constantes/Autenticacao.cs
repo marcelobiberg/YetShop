4bc2c44b572cdf9f1cd23ea3a08dcb0b9effab31
Marcelobiberg <biberg.marcelo@mail.com>
2020-09-04 15:37:41 -0300

    Alterada a constante DEFAULT_PASSWORD

--- YetShop/Yet.Core/Constantes/Autenticacao.cs
+++ YetShop/Yet.Core/Constantes/Autenticacao.cs
@@ -5,7 +5,7 @@
         public const string AUTH_KEY = "AuthKeyOfDoomThatMustBeAMinimumNumberOfBytes";
 
         // TODO: Não usar em produção
-        public const string DEFAULT_PASSWORD = "Pass@word1";
+        public const string DEFAULT_PASSWORD = "default";
 
         // TODO: Mudar para variável de ambiente
         public const string SECRET_KEY = "SecretKeyOfDoomThatMustBeAMinimumNumberOfBytes";
